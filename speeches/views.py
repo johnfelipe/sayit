30403e53fad23796083096c189f0b63296c4f223
rdryan <rdryan186@outlook.com>
2016-04-21 22:44:44 +0800

    add latest speech

--- speeches/views.py
+++ speeches/views.py
@@ -270,6 +270,7 @@ class InstanceView(NamespaceMixin, InstanceViewMixin, ListView):
         context['count_speakers'] = Speaker.objects.for_instance(self.request.instance).count()
         context['average_length'] = Speech.objects.for_instance(self.request.instance) \
             .annotate(length=Length('text')).aggregate(avg=Avg('length'))['avg']
+        context['last_speech']  = Speech.objects.all().order_by('start_date')[1]
         context['top_speakers'] = Speaker.objects.annotate(num_speeches=Count('speech')).order_by('-num_speeches')[:10]
         return context
 
