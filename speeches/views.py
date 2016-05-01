dd62ac39df3921c845da9101c5491742599f5ddf
rdryan <rdryan186@outlook.com>
2016-05-01 15:56:44 +0800

    add dummy question

--- speeches/views.py
+++ speeches/views.py
@@ -268,6 +268,7 @@ class InstanceView(NamespaceMixin, InstanceViewMixin, ListView):
         context['count_speeches'] = Speech.objects.for_instance(self.request.instance).visible(self.request).count()
         context['count_sections'] = Section.objects.for_instance(self.request.instance).count()
         context['count_speakers'] = Speaker.objects.for_instance(self.request.instance).count()
+        context['count_questions'] = Speaker.objects.for_instance(self.request.instance).count()  #TODO
         context['average_length'] = Speech.objects.for_instance(self.request.instance) \
             .annotate(length=Length('text')).aggregate(avg=Avg('length'))['avg']
         #context['last_speech']  = Speech.objects.all().order_by('-start_date')[1]
